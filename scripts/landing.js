b4e829c51f76e28d67472f6c1afb7746ca6dcf0c
Alex Lee <alexlee94@rocketmail.com>
2017-06-12 17:17:29 -1000

    Assignment 9 fix

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,14 +1,14 @@
 var pointsArray = document.getElementsByClassName('point');
 
 var revealPoint = function(point) {
-        points.style.opacity = 1;
-        points.style.transform = "scaleX(1) translateY(0)";
-        points.style.msTransform = "scaleX(1) translateY(0)";
-        points.style.WebkitTransform = "scaleX(1) translateY(0)";
+        point.style.opacity = 1;
+        point.style.transform = "scaleX(1) translateY(0)";
+        point.style.msTransform = "scaleX(1) translateY(0)";
+        point.style.WebkitTransform = "scaleX(1) translateY(0)";
     };
 
 var animatePoints = function(points) {
-    forEach(points, revealPoints);
+    forEach(points, revealPoint);
 };
 
 window.onload = function() {
@@ -16,11 +16,8 @@ window.onload = function() {
         animatePoints(pointsArray);
     }
 
-    var sellingPoints = document.getElementsByClassName('selling-points')[0];
-    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
-
     window.addEventListener('scroll', function(event) {
-        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+        if (pointsArray[0].getBoundingClientRect().top <= 500) {
             animatePoints(pointsArray);
         }
     });
